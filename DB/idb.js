aa1aa3a0dee3cd08e48b164556792aab2a6a2f83
CraziCoda <64246414+CraziCoda@users.noreply.github.com>
2020-08-10 11:21:23 +0000

    Update idb.js

--- DB/idb.js
+++ DB/idb.js
@@ -5,7 +5,6 @@ const IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction ||
 
 const IDBKeyRange = window.IDBKeyRange ||
     window.webkitIDBKeyRange || window.msIDBKeyRange;
-let btn = document.getElementsByClassName("btn_save");
 if (!indexedDB) {
     alert("Unsupported Browser");
 }
@@ -31,58 +30,6 @@ request.onupgradeneeded = (event) => {
         keyPath: "id",
         autoIncrement: true
     });
-};
-
-let preview = () => {
-    let objectStore = db.transaction("preview").objectStore("preview");
-    /*     let totalVal = document.getElementById('val');
-        let newVal = document.getElementById('netVal');
-     */
-    total = 0;
-    objectStore.openCursor().onsuccess = (event) => {
-        let cursor = event.target.result;
-        if (cursor) {
-            //preview code goes hwew
-            cursor.continue();
-        }
-
-    };
-
-};
-
-let add = () => {
-    let product = document.getElementById("iproduct");
-    let qty = document.getElementById("iqty");
-    let unit = document.getElementById("iunit");
-    let payment = document.getElementById("ipayment").value;
-
-    if (product.value != '' && qty.value != 0 && unit.value != 0) {
-
-        total = qty.value * unit.value;
-        let data = {
-            product: product.value,
-            quantity: qty.value,
-            unit: unit.value,
-            payment: payment,
-            total: total
-        };
-
-        let request = db.transaction(['preview'], 'readwrite')
-            .objectStore('preview').add(data);
-
-        request.onsuccess = (event) => {
-            console.log('Stored');
-            product.value = "";
-            qty.value = 0;
-            unit.value = 0;
-            //Other code goes here
-        };
-        request.onerror = (event) => {
-            console.log('Couldn\'t Stored');
-            //Other code goes here
-        };
-    }
-    preview();
 };
 
 
